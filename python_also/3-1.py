97e0ba4e5e668192dd254c4aaef6c1947e40dfdf
Eldar Akbarov <melancholywaterhole@gmail.com>
2021-07-26 12:35:46 +0300

    Приложение с Flask

--- python_also/3-1.py
+++
@@ -1,9 +0,0 @@
-import math as mt
-
-ang = int(input())
-
-if ang == 30:
-    s = float(mt.sin(mt.radians(ang)))
-    c = float(mt.cos(mt.radians(ang)))
-    print('sin угла 30 гр. равен ', s)
-    print('cos угла 30 гр. равен', c)
--- python_also/4-1.py
+++
@@ -1,7 +0,0 @@
-sum = 0
-number = 0
-while number <50:
-    number+=1
-    sum+=number
-
-print('Сумма равняется: ', sum)
